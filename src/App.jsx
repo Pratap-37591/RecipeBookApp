3244ece4335d8b71ee0c14fe24907b772e900de2
Pratap-37591 <prathaphb1310@gmail.com>
2023-06-20 19:37:21 +0530

    Project structure updated and some changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from "react";
 // import "./App.css";
 import axios from "axios";
-import Recipe from "./components/Recipe";
-import Paper from "@mui/material/Paper";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import InputBase from "@mui/material/InputBase";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
-import { Avatar, Typography } from "@mui/material";
-import { deepOrange } from "@mui/material/colors";
+import Recipe from "./components/RecipeBookCard/Recipe";
+import Navbar from "./components/navbar/Navbar";
+import { Grid } from "@mui/material";
 function App() {
   const APP_ID = "80a1c867";
   const APP_KEY = "efac35bc94d4646f76094d6ca9b8cc04";
@@ -40,57 +34,22 @@ function App() {
   };
   return (
     <div>
-      <Box
-        sx={{
-          flexGrow: 1,
-        }}
-      >
-        <AppBar position="static">
-          <Typography variant="h6" component="div">
-            RecipeBOOKApp
-          </Typography>
-          <Paper
-            style={{
-              margin: "0px auto",
-            }}
-            onSubmit={updateQuery}
-            component="form"
-            sx={{
-              p: "2px 4px",
-              display: "flex",
-              alignItems: "center",
-              width: 400,
-            }}
-          >
-            <InputBase
-              sx={{ ml: 1, flex: 1 }}
-              value={search}
-              onChange={handleSearch}
-              placeholder="Search Google Maps"
-              inputProps={{ "aria-label": "search google maps" }}
-            />
-            <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-              <SearchIcon />
-            </IconButton>
+     <Navbar handleSearch={handleSearch} updateQuery={updateQuery} />
 
-            <IconButton
-              color="primary"
-              sx={{ p: "5px" }}
-              aria-label="directions"
-            >
-              <Avatar sx={{ bgcolor: deepOrange[500] }}>P</Avatar>
-            </IconButton>
-          </Paper>
-        </AppBar>
-      </Box>
-      {recipes.map((recipe, index) => (
+     <div style={{margin: '30px'}}>
+     <Grid container>
+      {recipes.map((recipe, index) => ( 
+        <Grid item xs={3}>
         <Recipe
           key={index}
           title={recipe.recipe.label}
           image={recipe.recipe.image}
           ingredients={recipe.recipe.ingredients}
         />
+        </Grid>
       ))}
+      </Grid>
+      </div>
     </div>
   );
 }
