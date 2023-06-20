77e7a75d66dbaa5b05d10b451e797438db3e2680
Pratap-37591 <prathaphb1310@gmail.com>
2023-06-20 19:39:44 +0530

    Recipe card created

---
+++ src/components/RecipeBookCard/Recipe.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+
+const Recipe = ({ title, image, ingredients }) => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 325 , margin: '50px auto'}}>
+      <CardHeader title={title} />
+      <CardMedia component="img" height="194" image={image} alt={image.title} />
+      
+      <CardActions disableSpacing>
+      <div>Ingredients</div>
+
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          {ingredients.map((ingredient) => (
+            <Typography paragraph>{ingredient.text}</Typography>
+          ))}
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+};
+
+export default Recipe;
