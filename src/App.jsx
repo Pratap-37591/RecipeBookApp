58e03013656b505ac4b136256c8d9785ba0a9be8
Pratap-37591 <prathaphb1310@gmail.com>
2023-06-22 23:16:45 +0530

    React routing added for recipe view details component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import RecipeDetails from "./components/RecipeBookCard/RecipeDetails";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/details/:id" element={<RecipeDetails />} />
+      </Routes>
     </BrowserRouter>
   );
 }
