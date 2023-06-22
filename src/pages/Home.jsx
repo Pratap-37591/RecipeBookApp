b0ec3654f57c50dc43cfa8f78de8b5a8321d033b
Pratap-37591 <prathaphb1310@gmail.com>
2023-06-22 23:24:22 +0530

    Some changes done for home page component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,20 @@ const Home = () => {
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("");
 
+  const getRandomSearchQuery = () => {
+    const searchQueries = [
+      "banana",
+      "milk",
+      "vada",
+      "icecream",
+      "dessert",
+      "dose",
+      "pizza",
+    ];
+    const randomIndex = Math.floor(Math.random() * searchQueries.length);
+    return searchQueries[randomIndex];
+  };
+
   useEffect(() => {
     const getRecipe = async () => {
       try {
@@ -20,7 +34,7 @@ const Home = () => {
           `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
         );
         setRecipes(response.data.hits);
-        console.log(response.data);
+        console.log(response.data.hits);
       } catch (error) {
         console.error(error);
       }
@@ -29,11 +43,9 @@ const Home = () => {
     getRecipe();
   }, [query]);
 
-  const getRandomSearchQuery = () => {
-    const searchQueries = ["banana", "milk", "vada", "icecream", "dessert", "dose","pizza"];
-    const randomIndex = Math.floor(Math.random() * searchQueries.length);
-    return searchQueries[randomIndex];
-  };
+  if (!recipes) {
+    return <div>Is loading</div>;
+  }
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
@@ -46,19 +58,28 @@ const Home = () => {
 
   return (
     <div>
-      <Navbar handleSearch={handleSearch} updateQuery={updateQuery} />
+      <Navbar
+        handleSearch={handleSearch}
+        updateQuery={updateQuery}
+        search={search}
+      />
       <div style={{ margin: "30px", paddingTop: "4rem" }}>
         <Grid container>
-          {recipes.map((recipe, index) => (
-            <Grid item xs={3}>
-              <Recipe
-                key={index}
-                title={recipe.recipe.label}
-                image={recipe.recipe.image}
-                ingredients={recipe.recipe.ingredients}
-              />
-            </Grid>
-          ))}
+          {recipes.map((recipe, index) => {
+            const recipeId = recipe.recipe.uri.split("_").pop();
+            return (
+              <Grid item xs={3}>
+                <Recipe
+                  key={index}
+                  id={recipeId}
+                  title={recipe.recipe.label}
+                  image={recipe.recipe.image}
+                  ingredients={recipe.recipe.ingredients}
+                  ingredientLines={recipe.recipe.ingredientLines}
+                />
+              </Grid>
+            );
+          })}
         </Grid>
       </div>
     </div>
