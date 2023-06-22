338ffd0dcdd73ff1a9e8b778da1fff02b3c060fc
Pratap-37591 <prathaphb1310@gmail.com>
2023-06-22 07:18:07 +0530

    view and save button added and some modify done

--- src/components/RecipeBookCard/Recipe.jsx
+++ src/components/RecipeBookCard/Recipe.jsx
@@ -1,79 +1,79 @@
 import React from "react";
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Divider, List, ListItem, ListItemText, Tooltip } from "@mui/material";
-import BookmarkIcon from "@mui/icons-material/Bookmark";
+import StyledComponent from "styled-components";
+
+const ViewButton = StyledComponent.button`
+border: 2px solid #BF4F74;
+   background: #BF4F74;
+   color: white;
+   border-radius: 4px;
+   padding: 0.65em 1em;
+   font-family: 'Poppins', sans-serif;
+   font-size: 12px;
+`;
+
+const SaveButton = StyledComponent.button`
+   border: 2px solid green;
+   background: green;
+   color: white;
+   border-radius: 4px;
+   font-family: 'Poppins', sans-serif;
+   padding: 0.50em 1em;
+`;
 
 const Recipe = ({ title, image, ingredients }) => {
-  const [expanded, setExpanded] = React.useState(false);
+  // const [expanded, setExpanded] = React.useState(false);
 
-  const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-  })(({ theme, expand }) => ({
-    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  }));
+  // const ExpandMore = styled((props) => {
+  //   const { expand, ...other } = props;
+  //   return <IconButton {...other} />;
+  // })(({ theme, expand }) => ({
+  //   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  //   marginLeft: "auto",
+  //   transition: theme.transitions.create("transform", {
+  //     duration: theme.transitions.duration.shortest,
+  //   }),
+  // }));
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+  // const handleExpandClick = () => {
+  //   setExpanded(!expanded);
+  // };
 
   return (
     <Card sx={{ maxWidth: 325, margin: "0px auto 20px" }}>
+      <CardMedia component="img" height="194" image={image} alt={image.title} />
       <CardHeader
         titleTypographyProps={{ variant: "h6" }}
         sx={{
           textOverflow: "ellipsis",
-          whiteSpace: 'nowrap', 
-         
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textAlign: "center",
+          fontFamily: "Poppins, sans-serif",
         }}
         title={title}
       />
-      <CardMedia component="img" height="194" image={image} alt={image.title} />
-      <CardActions disableSpacing>
-        <Tooltip title="Save recipe">
-          <BookmarkIcon />
-        </Tooltip>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <Tooltip title="See more">
-            <ExpandMoreIcon />
-          </Tooltip>
-        </ExpandMore>
+      {/* <div>Ingredients: {ingredients[0].text}....</div> */}
+      {/* {console.log("Ingrede", title, ingredients[0].text)} */}
+      {/* {ingredients.map((ingredient, index) => (
+        <div>
+          {/* {console.log("ingredient", ingredient)} */}
+      {/* <span style={{ color: "black" }}>Recipe:{ingredient.text[8]}</span> */}
+      {/* </div> */}
+      {/* ))} */}
+
+      <CardActions
+        disableSpacing
+        sx={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <ViewButton>View Details</ViewButton>
+        <SaveButton>Save Receipe</SaveButton>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <div style={{ marginBottom: 10, fontSize: 18 }}>Ingredients </div>
-          <Divider variant="fullWidth" />
-          <List>
-            {ingredients.map((ingredient, index) => (
-              <ListItem key={index}>
-                <ListItemText primary={ingredient.text} />
-              </ListItem>
-            ))}
-          </List>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 };
 
 export default Recipe;
-// {ingredients.map((ingredient, index) => (
-//   <Typography key={index}>{ingredient.text}</Typography>
-// ))}
